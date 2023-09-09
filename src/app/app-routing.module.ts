3bf292508cd4b93bd4ae3b8882310a938428fc0a
Ricardo Aburto <ext.raburto@servipag.cl>
2023-09-09 12:44:59 -0300

    Init
    Inicio de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './shared/pages/home-page/home-page.component';
+import { AppComponent } from './app.component';
+import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'', component: HomePageComponent
+  },
+  {
+    path:'about',component: AboutPageComponent
+  },
+  {
+    path:'**', redirectTo:''
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
